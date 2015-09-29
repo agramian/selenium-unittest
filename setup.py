e94d8919e63ace38cd46496d03734780ea84eade
Abtin Gramian <abtin.gramian@gmail.com>
2015-09-28 17:28:21 -0700

    modifications for PyPI upload procedure
    - added MANIFEST.in
    - markdown to rst conversion of README in setup.py
    - removed description-file metadata from setup.cfg and replaced with
    lines for building universal wheel file

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
 
 setup(name='selenium_unittest',
-      version='0.1',
+      version='0.1.1',
       description='Selenium Unit Test Framework',
+      long_description=read_md('README.md'),
       author='Abtin Gramian',
       author_email='abtin.gramian@gmail.com',
       url='https://github.com/agramian/selenium-unittest',
@@ -14,7 +25,7 @@ setup(name='selenium_unittest',
         'selenium',
         'Appium-Python-Client==0.11'
       ],
-      download_url = 'https://github.com/agramian/selenium-unittest/tarball/v0.1',
+      download_url = 'https://github.com/agramian/selenium-unittest/tarball/v0.1.1',
       keywords = ['selenium', 'unittest', 'unit', 'test', 'testing'],
       classifiers = [],
      )
